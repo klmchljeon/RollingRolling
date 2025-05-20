d73300a1b2f925e80eb8dac36c0f3eb088ef8b08
hyeok0pct <mini332791@naver.com>
2025-05-20 18:06:26 +0900

    과녁 좌표 저장 작업

--- Assets/MiniGame2/Scripts/M2_GenerateTarget.cs
+++ Assets/MiniGame2/Scripts/M2_GenerateTarget.cs
@@ -7,10 +7,11 @@ public class M2_GenerateTarget : MonoBehaviour
 {
     
     public GameObject target; // 타겟 오브젝트를 드래그하여 연결합니다.
-
+    public M2_AngleManager angleManager; // 타겟 각도 포인트 스크립트를 드래그하여 연결합니다.
     float MakeTarget(float exceptAngle)
     {
         float angle = Random.Range(0, 360); // 0도에서 360도 사이의 랜덤 각도를 생성합니다.
+
         while (true)
         {
             if (exceptAngle == -1) break; // 예외 각도가 -1이면 무한 루프를 종료합니다.
@@ -25,7 +26,9 @@ public class M2_GenerateTarget : MonoBehaviour
             else
             {
                 angle = Random.Range(0, 360); // 예외 각도와의 차이가 30도 이하이면 다시 랜덤 각도를 생성합니다.
+                angleManager.SetTmp(angle); // 타겟 각도 포인트를 체크합니다.
             }
+                            
         }
        
         // 타겟을 생성할 위치를 랜덤으로 결정합니다.
@@ -36,20 +39,24 @@ public class M2_GenerateTarget : MonoBehaviour
         Debug.Log(Newtarget.transform.localScale);
         Newtarget.transform.SetParent(transform); // 부모 오브젝트로 설정합니다.
         Newtarget.transform.position = randomPosition;
+
+        
         return angle;
+        
     }
 
 
 
-    // Update is called once per frame
-    void Update()
+    public void GeneratingTarget()
     {
-    
-    }
+        int targetNumber = Random.Range(1, 3); // 1 또는 2를 랜덤으로 선택합니다.
 
-    public void MakeTargetTwice()
-    {
-        float tmp = MakeTarget(-1);
-        MakeTarget(tmp);
+        if (targetNumber == 1)
+            MakeTarget(-1); // -1을 예외 각도로 사용하여 타겟을 생성합니다.
+        else
+        {
+            float tmp = MakeTarget(-1);
+            MakeTarget(tmp);
+        }
     }
 }
